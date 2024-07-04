4f2a8359749f051bcfada9381fa1da2d25719fe6
rlouvrie <rlouvrie@student.42.fr>
2024-07-04 13:48:13 +0200

    strdup ok

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -3,14 +3,27 @@
 ; char	*ft_strdup(const char *s);
 ; rdi = const char *s
 
-extern malloc
+extern ft_strlen, ft_strcpy, malloc
 
 global ft_strdup
 
 section .text
 
 ft_strdup:
-	call ft_strlen
-	mv rdx, rax
-	
+	push rdi
+    call ft_strlen
+    inc rax
+    mov rdi, rax
+    call malloc WRT ..plt
+    cmp rax, 0
+    je .mem_fail
+    mov rdi, rax
+	pop rsi
+    call ft_strcpy
+    ret
+
+.mem_fail:
+    xor rax, rax
+    ret
+
 section .note.GNU-stack noalloc noexec nowrite progbits
