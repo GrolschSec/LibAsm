739951dbd7d1f8c13ed80af63a32c928a8fb6756
rlouvrie <rlouvrie@student.42.fr>
2024-07-08 15:59:54 +0200

    libasm h ok

--- libasm.h
+++ libasm.h
@@ -6,17 +6,13 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 16:56:21 by rlouvrie          #+#    #+#             */
-/*   Updated: 2024/07/03 17:44:41 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2024/07/08 15:59:16 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBASM_H
 # define LIBASM_H
-# define RED_COLOR "\033[0;31m"
-# define GREEN_COLOR "\033[0;32m"
-# define RESET_COLOR "\033[0m"
 # include <sys/types.h>
-# include <errno.h>
 
 /* srcs/ft_strcmp.s */
 int		ft_strcmp(const char *s1, const char *s2);
