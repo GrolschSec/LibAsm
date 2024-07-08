6abefcfddb5117841f1332c8ceef2a05d8a215bc
rlouvrie <rlouvrie@student.42.fr>
2024-07-08 16:01:31 +0200

    main ok

--- main.c
+++ main.c
@@ -5,58 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rlouvrie <rlouvrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/29 16:53:36 by rlouvrie          #+#    #+#             */
-/*   Updated: 2024/07/04 13:51:49 by rlouvrie         ###   ########.fr       */
+/*   Created: 2024/07/08 15:25:32 by rlouvrie          #+#    #+#             */
+/*   Updated: 2024/07/08 15:53:52 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libasm.h"
 #include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
 #include <stdlib.h>
+#include <strings.h>
 
 int	main(void)
 {
-	const char	*ok = GREEN_COLOR "OK" RESET_COLOR;
-    const char	*ko = RED_COLOR "KO" RESET_COLOR;
+	char	*str;
+	char	buf[100];
 
-    printf("********** ft_strlen **********\n");
-    printf("1: %s ", ft_strlen("Hello World") == 11 ? ok : ko);
-    printf("2: %s ", ft_strlen("") == 0 ? ok : ko);
-	printf("\n\n");
-
-	printf("********** ft_strcpy **********\n");
-	char s1[100] = "Hello World";
-	char s2[100] = "";
-	printf("1: %s ", strcmp(ft_strcpy(s2, s1), s1) == 0 ? ok : ko);
-	printf("2: %s ", strcmp(ft_strcpy(s2, ""), "") == 0 ? ok : ko);
-	printf("\n\n");
-	
-	printf("********** ft_strcmp **********\n");
-	printf("1: %s ", ft_strcmp("Hello World", "Hello World") == 0 ? ok : ko);
-	printf("2: %s ", ft_strcmp("Hello World", "Hello") > 0 ? ok : ko);
-	printf("3: %s ", ft_strcmp("Hello", "Hello World") < 0 ? ok : ko);
-	printf("\n\n");
-
-	printf("********** ft_write  **********\n");
-	ssize_t bytes;
-	errno = 0;
-	bytes = ft_write(STDOUT_FILENO, "Hello, ft_write!\n", 17);
-	if (bytes == 17 && errno == 0) {
-		printf("1: %s ", ok);
-	} else {
-		printf("1: %s ", ko);
-	}
-
-	errno = 0;
-	bytes = ft_write(-1, "This should fail.\n", 18);
-	if (bytes == -1 && errno != 0) {
-    	printf("2: %s ", ok);
-	} else {
-    	printf("2: %s ", ko);
-	}
-	printf("\n\n");
+	printf("ft_strlen(\"Hello World\"): %ld\n", ft_strlen("Hello World"));
+	str = ft_strdup("Hello World");
+	printf("ft_strdup(\"Hello World\"): %s\n", str);
+	if (str)
+		free(str);
+	printf(
+		"ft_strcmp(\"Hello World\", \"Hello World\"): %d\n",
+		ft_strcmp("Hello World", "Hello World")
+		);
+	printf(
+		"ft_strcpy(buf, \"Hello World\"): %s\n",
+		ft_strcpy(buf, "Hello World")
+		);
+	ft_write(1, "ft_write wrote this line\n", 25);
+	bzero(buf, 100);
+	ft_read(0, buf, 100);
+	printf("ft_read read this line: %s", buf);
 	return (0);
 }
